d0f0508d62de9c9366a8edcfdf8c39a6ccbfb645
Denis Goncharov <imperialeaglex@gmail.com>
2021-08-12 00:45:32 +0300

    change typeorm ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,7 @@ import { User } from '@app/users/entities/user.entity';
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: false,
       entities: [Message, User],
-      ssl: process.env.NODE_ENV === 'prod',
+      ssl: { rejectUnauthorized: false },
     }),
     ChatModule,
     AuthModule,
